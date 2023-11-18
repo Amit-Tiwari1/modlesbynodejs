b2c08ab9ad8d49f429a23fab432a12a23c053fde
Amit Tiwari <70047168+Amit-Tiwari1@users.noreply.github.com>
2023-11-18 11:13:36 +0530

    Create order.models.js
    this part of ecommerce management system

---
+++ order.models.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+// indivisual product order schema
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [orderItemSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'CANCELLED', 'DELIEVERED'],
+      default: 'PENDING',
+    },
+  },
+  { timestaps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
