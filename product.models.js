7e77148b7add373fae71848654add9b02a1384f5
Amit Tiwari <70047168+Amit-Tiwari1@users.noreply.github.com>
2023-11-18 11:14:30 +0530

    Create product.models.js
    this is part of ecommerce management system

---
+++ product.models.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      required: true,
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      type: string,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      default: 0,
+      type: Number,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.objectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model('Product', productSchema);
